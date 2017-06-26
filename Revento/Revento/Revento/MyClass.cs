ff3b698ab4427f4d01b361a2dba4efc0df81b925
JurrianK <jurriankole1997@gmail.com>
2017-06-26 10:20:42 +0200

    XML reader

--- Revento/Revento/Revento/MyClass.cs
+++ Revento/Revento/Revento/MyClass.cs
@@ -1,14 +1,26 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
 
 namespace Revento
 {
     /// <summary>
     /// Shared code
     /// </summary>
-	public class MyClass {
-		public MyClass ()
-		{
+	public class MyClass
+    {
+        public MyClass()
+        {
 
         }
-	}
+
+        public static void RetrieveXML()
+        {
+            XDocument xdoc = XDocument.Load("events.xml");
+
+            var query = from c in xdoc.Root.Descendants("event")
+                        select c.Element("title").Value;
+        }
+    }
 }
