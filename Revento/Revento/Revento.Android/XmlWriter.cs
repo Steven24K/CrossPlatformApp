7bf4f0defe822921ad9464db011fc1d000c985b5
Steven 24k <s.koerts2@gmail.com>
2017-06-30 13:43:40 +0200

    Fixed Xml writer for events

--- Revento/Revento/Revento.Android/XmlWriter.cs
+++ Revento/Revento/Revento.Android/XmlWriter.cs
@@ -16,13 +16,13 @@ namespace Revento.Droid
 {
     public class XmlWriter
     {
-        public static void AddEvent(string file, string title, string description, string date, string adress)
+        public static void AddEvent(string file, string title, string date, string description, string adress, string website)
         {
             XmlDocument xd = new XmlDocument();
             xd.Load(file);
             XmlNode nl = xd.SelectSingleNode("resources");
             XmlDocument xd2 = new XmlDocument();
-            xd2.LoadXml("<events><title>" + title + "</title><description>" + description + "</description><date>" + date + "</date><adress>" + adress + "</adress></events>");
+            xd2.LoadXml("<events><title>" + title + "</title><date>" + date + "</date><description>" + description + "</description><adress>" + adress + "</adress><website>" + website + "</website></events>");
             XmlNode n = xd.ImportNode(xd2.FirstChild, true);
             nl.AppendChild(n);
             xd.Save(file);
