8fa63df8a4f53939cc4ba38fa730a8b37abd6b34
JurrianK <jurriankole1997@gmail.com>
2017-06-30 13:38:34 +0200

    Contact activity v1.1

--- Revento/Revento/Revento.Android/ContactActivity.cs
+++ Revento/Revento/Revento.Android/ContactActivity.cs
@@ -1,20 +1,37 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.OS;
+using Android.Util;
 using Android.Views;
+using Android.Widget;
 
 namespace Revento.Droid
 {
     [Activity(Label = "Contact")]
     public class ContactForm : Activity
     {
+        private TextView _dateDisplay;
+        private Button _dateSelect;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Contact);
 
+            _dateDisplay = FindViewById<TextView>(Resource.Id.date_display);
+            _dateSelect = FindViewById<Button>(Resource.Id.date_button);
+            _dateSelect.Click += DateSelect_OnClick;
+
             // Toon een backbutton in de actionbar
             ActionBar.SetHomeButtonEnabled(true);
             ActionBar.SetDisplayHomeAsUpEnabled(true);
+        }
+
+        public void DateSelect_OnClick(object sender, EventArgs eventArgs)
+        {
+            DatePickerFragment frag = DatePickerFragment.NewInstance(delegate(DateTime time) { _dateDisplay.Text = time.ToLongDateString(); });
+
+            frag.Show(FragmentManager, DatePickerFragment.TAG);
         }
 
         // Override de functionaliteit van de backbutton zodat het teruggaat naar de mainactivity
@@ -29,6 +46,38 @@ namespace Revento.Droid
                 default:
                     return base.OnOptionsItemSelected(item);
             }
+        }
+    }
+
+    public class DatePickerFragment : DialogFragment, DatePickerDialog.IOnDateSetListener
+    {
+        public static readonly string TAG = "X:" + typeof(DatePickerFragment).Name.ToUpper();
+
+        private Action<DateTime> _dateSelectHandler = delegate { };
+
+        public static DatePickerFragment NewInstance(Action<DateTime> onDateSelected)
+        {
+            DatePickerFragment frag = new DatePickerFragment();
+            frag._dateSelectHandler = onDateSelected;
+            return frag;
+        }
+
+        public override Dialog OnCreateDialog(Bundle bundle)
+        {
+            DateTime currently = DateTime.Now;
+
+            DatePickerDialog dialog = new DatePickerDialog(Activity, this, currently.Year, currently.Month, currently.Day);
+
+            return dialog;
+        }
+
+        public void OnDateSet(DatePicker view, int year, int month, int day)
+        {
+            DateTime selectedDate = new DateTime(year, month + 1, day);
+
+            Log.Debug(TAG, selectedDate.ToLongDateString());
+
+            _dateSelectHandler(selectedDate);
         }
     }
 }
